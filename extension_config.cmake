a9e8fd11814300037faede4f2ed2f08a9c29824a
Carlo Piovesan <piovesan.carlo@gmail.com>
2025-06-03 11:36:32 +0200

    WIP: Various changes required to bring back wasm compilation

--- extension_config.cmake
+++ extension_config.cmake
@@ -6,31 +6,3 @@ duckdb_extension_load(iceberg
     LOAD_TESTS
     LINKED_LIBS "../../vcpkg_installed/wasm32-emscripten/lib/*.a"
 )
-
-
-if (NOT ${EMSCRIPTEN})
-duckdb_extension_load(tpch)
-duckdb_extension_load(icu)
-duckdb_extension_load(avro
-        LOAD_TESTS
-        GIT_URL https://github.com/duckdb/duckdb_avro
-        GIT_TAG 141c70afa871d437bfdc689b826baf55409b1f72
-)
-endif()
-
-################## AWS
-if (NOT MINGW AND NOT ${EMSCRIPTEN})
-    duckdb_extension_load(aws
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG main
-    )
-endif ()
-
-if (NOT ${EMSCRIPTEN})
-duckdb_extension_load(httpfs
-        GIT_URL https://github.com/duckdb/duckdb-httpfs
-        GIT_TAG 7b09112ad257249130375c0841d962eecb85662e
-        INCLUDE_DIR extension/httpfs/include
-)
-endif ()
