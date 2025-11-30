4cebc5d9c09cc6757eab780c4efb43c11a3bf694
djouallah <djouallah@users.noreply.github.com>
2025-11-30 22:28:49 +1000

    Simplify extension_config.cmake to only build iceberg and required avro dependency
    Removed unnecessary extensions (tpch, icu, ducklake, aws) to speed up build time

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,4 +1,6 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
+
+# Load only avro (required dependency for iceberg)
 if (NOT EMSCRIPTEN)
 duckdb_extension_load(avro
 		LOAD_TESTS
@@ -12,21 +14,3 @@ duckdb_extension_load(iceberg
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
 )
-
-if (NOT EMSCRIPTEN)
-duckdb_extension_load(tpch)
-duckdb_extension_load(icu)
-duckdb_extension_load(ducklake
-        LOAD_TESTS
-        GIT_URL https://github.com/duckdb/ducklake
-        GIT_TAG f134ad86f2f6e7cdf4133086c38ecd9c48f1a772
-)
-
-if (NOT MINGW)
-    duckdb_extension_load(aws
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 18803d5e55b9f9f6dda5047d0fdb4f4238b6801d
-    )
-endif()
-endif()
