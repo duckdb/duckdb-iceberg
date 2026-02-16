96ce92d8c2f424df748817754bee7172b36c5394
Tishj <t_b@live.nl>
2026-02-16 10:29:59 +0100

    more undoing

--- src/include/storage/iceberg_transaction_data.hpp
+++ src/include/storage/iceberg_transaction_data.hpp
@@ -20,7 +20,9 @@ struct IcebergCreateTableRequest;
 
 struct IcebergTransactionData {
 public:
-	IcebergTransactionData(ClientContext &context, IcebergTableInformation &table_info);
+	IcebergTransactionData(ClientContext &context, IcebergTableInformation &table_info)
+	    : context(context), table_info(table_info), is_deleted(false) {
+	}
 
 public:
 	void CreateManifestListEntry(IcebergAddSnapshot &add_snapshot, IcebergTableMetadata &table_metadata,
