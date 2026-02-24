d1678cdc41994c6268311151214598589a9afc25
Tmonster <tom@ebergen.com>
2026-02-24 22:51:53 +0100

    move some stuff around

--- src/iceberg_manifest.cpp
+++ src/iceberg_manifest.cpp
@@ -136,6 +136,9 @@ Value IcebergDataFile::ToValue(const LogicalType &type) const {
 				if (entry.source_type == LogicalTypeId::TIMESTAMP) {
 					partition_result_type = LogicalType::BIGINT;
 				}
+				if (entry.source_type == LogicalTypeId::DATE) {
+					partition_result_type = LogicalType::BIGINT;
+				}
 				if (entry.source_type.IsNested()) {
 					throw InvalidInputException("Cannot use identify partition on a nested column");
 				}
