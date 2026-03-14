2794936f4f26d92e9df0568e8f2a4adfddef3163
Mathias Åhsberg <mathias.ahsberg@gmail.com>
2026-03-14 19:29:14 +0100

    fix: glue default catalog

--- src/catalog_api.cpp
+++ src/catalog_api.cpp
@@ -474,7 +474,9 @@ rest_api_objects::LoadTableResult IRCAPI::CommitNewTable(ClientContext &context,
 rest_api_objects::CatalogConfig IRCAPI::GetCatalogConfig(ClientContext &context, IcebergCatalog &catalog) {
 	auto url_builder = catalog.GetBaseUrl();
 	url_builder.AddPathComponent("config");
-	url_builder.SetParam("warehouse", catalog.warehouse);
+	if (catalog.warehouse != ":") {
+		url_builder.SetParam("warehouse", catalog.warehouse);
+	}
 	string body = "";
 	HTTPHeaders headers(*context.db);
 	auto response = catalog.auth_handler->Request(RequestType::GET_REQUEST, context, url_builder, headers, body);
