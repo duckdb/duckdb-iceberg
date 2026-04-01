707a5b8e0f4eec3eef2aefa586e78af6d0cf6a3e
Tishj <t_b@live.nl>
2026-04-01 23:41:44 +0200

    use unified vector format in the ManifestReader::ReadChunk

--- src/planning/metadata_io/manifest/iceberg_manifest_reader.cpp
+++ src/planning/metadata_io/manifest/iceberg_manifest_reader.cpp
@@ -20,74 +20,120 @@ void ManifestReader::Read() {
 	ScanInternal();
 }
 
-static unordered_map<int32_t, Value> GetBounds(Vector &bounds, idx_t index) {
-	auto &bounds_child = ListVector::GetEntry(bounds);
-	auto keys = FlatVector::GetData<int32_t>(*StructVector::GetEntries(bounds_child)[0]);
-	auto &values = *StructVector::GetEntries(bounds_child)[1];
-	auto bounds_list = FlatVector::GetData<list_entry_t>(bounds);
-
+static unordered_map<int32_t, Value> GetBounds(RecursiveUnifiedVectorFormat &format, idx_t i) {
 	unordered_map<int32_t, Value> parsed_bounds;
 
-	auto &validity = FlatVector::Validity(bounds);
+	auto &validity = format.unified.validity;
+	auto index = format.unified.sel->get_index(i);
 	if (!validity.RowIsValid(index)) {
 		return parsed_bounds;
 	}
+	auto bounds_data = format.unified.GetData<list_entry_t>(format.unified);
+	auto list_entry = bounds_data[index];
 
-	auto list_entry = bounds_list[index];
+	auto &inner_struct_format = format.children[0];
+	auto &keys_format = inner_struct_format.children[0].unified;
+	auto &values_format = inner_struct_format.children[1].unified;
+	auto keys_data = keys_format.GetData<int32_t>(keys_format);
+	auto values_data = values_format.GetData<string_t>(values_format);
 	for (idx_t j = 0; j < list_entry.length; j++) {
-		auto list_idx = list_entry.offset + j;
-		parsed_bounds[keys[list_idx]] = values.GetValue(list_idx);
+		auto keys_idx = keys_format.sel->get_index(list_entry.offset + j);
+		auto values_idx = values_format.sel->get_index(list_entry.offset + j);
+
+		auto value = Value(LogicalType::BLOB);
+		if (values_format.validity.RowIsValid(values_idx)) {
+			auto &str = values_data[values_idx];
+			value = Value::BLOB(const_data_ptr_cast(str.GetData()), str.GetSize());
+		}
+		parsed_bounds[keys_data[keys_idx]] = value;
 	}
 	return parsed_bounds;
 }
 
-static unordered_map<int32_t, int64_t> GetCounts(Vector &counts, idx_t index) {
-	auto &counts_child = ListVector::GetEntry(counts);
-	auto keys = FlatVector::GetData<int32_t>(*StructVector::GetEntries(counts_child)[0]);
-	auto values = FlatVector::GetData<int64_t>(*StructVector::GetEntries(counts_child)[1]);
-	auto counts_list = FlatVector::GetData<list_entry_t>(counts);
-
+static unordered_map<int32_t, int64_t> GetCounts(const char *name, RecursiveUnifiedVectorFormat &format, idx_t i) {
 	unordered_map<int32_t, int64_t> parsed_counts;
 
-	auto &validity = FlatVector::Validity(counts);
+	auto &validity = format.unified.validity;
+	auto index = format.unified.sel->get_index(i);
 	if (!validity.RowIsValid(index)) {
 		return parsed_counts;
 	}
+	auto bounds_data = format.unified.GetDataUnsafe<list_entry_t>(format.unified);
+	auto list_entry = bounds_data[index];
 
-	auto list_entry = counts_list[index];
+	auto &inner_struct_format = format.children[0];
+	auto &keys_format = inner_struct_format.children[0].unified;
+	auto &values_format = inner_struct_format.children[1].unified;
+	auto keys_data = keys_format.GetData<int32_t>(keys_format);
+	auto values_data = values_format.GetData<int64_t>(values_format);
 	for (idx_t j = 0; j < list_entry.length; j++) {
-		auto list_idx = list_entry.offset + j;
-		parsed_counts[keys[list_idx]] = values[list_idx];
+		auto keys_idx = keys_format.sel->get_index(list_entry.offset + j);
+		auto values_idx = values_format.sel->get_index(list_entry.offset + j);
+
+		auto key = keys_data[keys_idx];
+		if (!values_format.validity.RowIsValid(values_idx)) {
+			throw InvalidConfigurationException("'%s' map's value for key '%d' is NULL", name, key);
+		}
+		parsed_counts[key] = values_data[values_idx];
 	}
 	return parsed_counts;
 }
 
 template <class T>
-static vector<T> GetListTemplated(Vector &item, idx_t index) {
+static vector<T> GetListTemplated(const char *name, RecursiveUnifiedVectorFormat &item_format, idx_t i) {
 	vector<T> result;
+	auto &format = item_format.unified;
 
-	if (!FlatVector::Validity(item).RowIsValid(index)) {
+	auto &validity = format.validity;
+	auto index = format.sel->get_index(i);
+	if (!validity.RowIsValid(index)) {
 		return result;
 	}
-	auto &item_child = ListVector::GetEntry(item);
-	auto item_data = FlatVector::GetData<T>(item_child);
-	auto item_list = FlatVector::GetData<list_entry_t>(item);
-	auto list_entry = item_list[index];
+
+	auto &child_format = item_format.children[0].unified;
+	auto child_data = child_format.GetDataUnsafe<T>(child_format);
+
+	auto list_data = format.GetDataUnsafe<list_entry_t>(format);
+	auto list_entry = list_data[index];
 
 	for (idx_t j = 0; j < list_entry.length; j++) {
-		auto list_idx = list_entry.offset + j;
-		result.push_back(item_data[list_idx]);
+		auto list_idx = child_format.sel->get_index(list_entry.offset + j);
+		if (!child_format.validity.RowIsValid(list_idx)) {
+			throw InvalidConfigurationException("'%s' list contains NULL", name);
+		}
+		result.push_back(child_data[list_idx]);
 	}
 
 	return result;
 }
 
-static vector<int32_t> GetEqualityIds(Vector &equality_ids, idx_t index) {
-	return GetListTemplated<int32_t>(equality_ids, index);
+static vector<int32_t> GetEqualityIds(RecursiveUnifiedVectorFormat &equality_ids, idx_t i) {
+	return GetListTemplated<int32_t>("equality_ids", equality_ids, i);
 }
 
-static vector<int64_t> GetSplitOffsets(Vector &split_offsets, idx_t index) {
-	return GetListTemplated<int64_t>(split_offsets, index);
+static vector<int64_t> GetSplitOffsets(RecursiveUnifiedVectorFormat &split_offsets, idx_t i) {
+	return GetListTemplated<int64_t>("split_offsets", split_offsets, i);
+}
+
+template <class T>
+T ReadRequiredField(const char *name, UnifiedVectorFormat &format, idx_t i) {
+	auto data = format.GetDataUnsafe<T>(format);
+	auto index = format.sel->get_index(i);
+	if (!format.validity.RowIsValid(index)) {
+		throw InvalidConfigurationException("required field '%s' is NULL!", name);
+	}
+	return data[index];
+}
+
+template <class T>
+bool ReadOptionalField(UnifiedVectorFormat &format, idx_t i, T &result) {
+	auto data = format.GetDataUnsafe<T>(format);
+	auto index = format.sel->get_index(i);
+	if (format.validity.RowIsValid(index)) {
+		result = data[index];
+		return true;
+	}
+	return false;
 }
 
 void ManifestReader::ReadChunk(DataChunk &chunk, const map<idx_t, LogicalType> &partition_field_id_to_type,
@@ -101,13 +147,8 @@ void ManifestReader::ReadChunk(DataChunk &chunk, const map<idx_t, LogicalType> &
 	idx_t vector_index = 0;
 	auto &status = chunk.data[vector_index++];
 	auto &snapshot_id = chunk.data[vector_index++];
-
 	auto &sequence_number = chunk.data[vector_index++];
-	auto &sequence_number_validity = FlatVector::Validity(sequence_number);
-
 	auto &file_sequence_number = chunk.data[vector_index++];
-	auto &file_sequence_number_validity = FlatVector::Validity(file_sequence_number);
-
 	auto &data_file = chunk.data[vector_index++];
 
 	idx_t entry_index = 0;
@@ -134,43 +175,86 @@ void ManifestReader::ReadChunk(DataChunk &chunk, const map<idx_t, LogicalType> &
 	if (iceberg_version >= 3) {
 		first_row_id = *data_file_entries[entry_index++];
 	}
+
+	UnifiedVectorFormat referenced_data_file_format;
 	optional_ptr<Vector> referenced_data_file;
 	if (iceberg_version >= 2) {
 		referenced_data_file = *data_file_entries[entry_index++];
+		referenced_data_file->ToUnifiedFormat(count, referenced_data_file_format);
 	}
+	UnifiedVectorFormat content_offset_format;
 	optional_ptr<Vector> content_offset;
+
+	UnifiedVectorFormat content_size_in_bytes_format;
 	optional_ptr<Vector> content_size_in_bytes;
 	if (iceberg_version >= 3) {
 		content_offset = *data_file_entries[entry_index++];
 		content_size_in_bytes = *data_file_entries[entry_index++];
+
+		content_offset->ToUnifiedFormat(count, content_offset_format);
+		content_size_in_bytes->ToUnifiedFormat(count, content_size_in_bytes_format);
 	}
 
-	auto status_data = FlatVector::GetData<int32_t>(status);
+	UnifiedVectorFormat status_format;
+	status.ToUnifiedFormat(count, status_format);
 
-	auto &snapshot_id_validity = FlatVector::Validity(snapshot_id);
-	auto snapshot_id_data = FlatVector::GetData<int64_t>(snapshot_id);
+	UnifiedVectorFormat snapshot_id_format;
+	snapshot_id.ToUnifiedFormat(count, snapshot_id_format);
 
-	auto sequence_number_data = FlatVector::GetData<int64_t>(sequence_number);
-	auto file_sequence_number_data = FlatVector::GetData<int64_t>(file_sequence_number);
+	UnifiedVectorFormat sequence_number_format;
+	sequence_number.ToUnifiedFormat(count, sequence_number_format);
 
-	auto &sort_order_id_validity = FlatVector::Validity(sort_order_id);
-	auto sort_order_id_data = FlatVector::GetData<int32_t>(sort_order_id);
+	UnifiedVectorFormat file_sequence_number_format;
+	file_sequence_number.ToUnifiedFormat(count, file_sequence_number_format);
 
-	int32_t *content_data = nullptr;
-	int64_t *first_row_id_data = nullptr;
-	optional_ptr<ValidityMask> first_row_id_validity;
+	UnifiedVectorFormat sort_order_id_format;
+	sort_order_id.ToUnifiedFormat(count, sort_order_id_format);
+
+	UnifiedVectorFormat content_format;
+	UnifiedVectorFormat first_row_id_format;
+
 	if (iceberg_version >= 2) {
-		content_data = FlatVector::GetData<int32_t>(*content);
+		content->ToUnifiedFormat(count, content_format);
 	}
 	if (iceberg_version >= 3) {
-		first_row_id_data = FlatVector::GetData<int64_t>(*first_row_id);
-		first_row_id_validity = FlatVector::Validity(*first_row_id);
+		first_row_id->ToUnifiedFormat(count, first_row_id_format);
 	}
-	auto file_path_data = FlatVector::GetData<string_t>(file_path);
-	auto file_format_data = FlatVector::GetData<string_t>(file_format);
-	// auto partition_data = FlatVector::GetData<int32_t>(partition);
-	auto record_count_data = FlatVector::GetData<int64_t>(record_count);
-	auto file_size_in_bytes_data = FlatVector::GetData<int64_t>(file_size_in_bytes);
+
+	UnifiedVectorFormat file_path_format;
+	file_path.ToUnifiedFormat(count, file_path_format);
+
+	UnifiedVectorFormat file_format_format;
+	file_format.ToUnifiedFormat(count, file_format_format);
+
+	UnifiedVectorFormat record_count_format;
+	record_count.ToUnifiedFormat(count, record_count_format);
+
+	UnifiedVectorFormat file_size_in_bytes_format;
+	file_size_in_bytes.ToUnifiedFormat(count, file_size_in_bytes_format);
+
+	RecursiveUnifiedVectorFormat lower_bounds_format;
+	Vector::RecursiveToUnifiedFormat(lower_bounds, count, lower_bounds_format);
+
+	RecursiveUnifiedVectorFormat upper_bounds_format;
+	Vector::RecursiveToUnifiedFormat(upper_bounds, count, upper_bounds_format);
+
+	RecursiveUnifiedVectorFormat column_sizes_format;
+	Vector::RecursiveToUnifiedFormat(column_sizes, count, column_sizes_format);
+
+	RecursiveUnifiedVectorFormat value_counts_format;
+	Vector::RecursiveToUnifiedFormat(value_counts, count, value_counts_format);
+
+	RecursiveUnifiedVectorFormat null_value_counts_format;
+	Vector::RecursiveToUnifiedFormat(null_value_counts, count, null_value_counts_format);
+
+	RecursiveUnifiedVectorFormat nan_value_counts_format;
+	Vector::RecursiveToUnifiedFormat(nan_value_counts, count, nan_value_counts_format);
+
+	RecursiveUnifiedVectorFormat split_offsets_format;
+	Vector::RecursiveToUnifiedFormat(split_offsets, count, split_offsets_format);
+
+	RecursiveUnifiedVectorFormat equality_ids_format;
+	Vector::RecursiveToUnifiedFormat(equality_ids, count, equality_ids_format);
 
 	vector<std::pair<int32_t, reference<Vector>>> partition_vectors;
 	if (partition.GetType().id() != LogicalTypeId::SQLNULL) {
@@ -183,76 +267,95 @@ void ManifestReader::ReadChunk(DataChunk &chunk, const map<idx_t, LogicalType> &
 	}
 
 	for (idx_t i = 0; i < count; i++) {
-		idx_t index = i;
 		IcebergManifestEntry entry;
 
-		entry.status = (IcebergManifestEntryStatusType)status_data[index];
+		entry.status = (IcebergManifestEntryStatusType)ReadRequiredField<int32_t>("status", status_format, i);
 
 		auto &data_file = entry.data_file;
-		data_file.file_path = file_path_data[index].GetString();
-		data_file.file_format = file_format_data[index].GetString();
-		data_file.record_count = record_count_data[index];
-		data_file.file_size_in_bytes = file_size_in_bytes_data[index];
 
-		data_file.lower_bounds = GetBounds(lower_bounds, index);
-		data_file.upper_bounds = GetBounds(upper_bounds, index);
-		data_file.column_sizes = GetCounts(column_sizes, index);
-		data_file.value_counts = GetCounts(value_counts, index);
-		data_file.null_value_counts = GetCounts(null_value_counts, index);
-		data_file.nan_value_counts = GetCounts(nan_value_counts, index);
+		auto file_path_index = file_path_format.sel->get_index(i);
+		if (!file_path_format.validity.RowIsValid(file_path_index)) {
+			throw InvalidConfigurationException("required field 'file_path' is NULL!");
+		}
+		data_file.file_path = ReadRequiredField<string_t>("file_path", file_path_format, i).GetString();
+		data_file.file_format = ReadRequiredField<string_t>("file_format", file_format_format, i).GetString();
+		data_file.record_count = ReadRequiredField<int64_t>("record_count", record_count_format, i);
+		data_file.file_size_in_bytes = ReadRequiredField<int64_t>("file_size_in_bytes", file_size_in_bytes_format, i);
 
-		if (referenced_data_file && FlatVector::Validity(*referenced_data_file).RowIsValid(index)) {
-			data_file.referenced_data_file = FlatVector::GetData<string_t>(*referenced_data_file)[index].GetString();
+		data_file.lower_bounds = GetBounds(lower_bounds_format, i);
+		data_file.upper_bounds = GetBounds(upper_bounds_format, i);
+		data_file.column_sizes = GetCounts("column_sizes", column_sizes_format, i);
+		data_file.value_counts = GetCounts("value_counts", value_counts_format, i);
+		data_file.null_value_counts = GetCounts("null_value_counts", null_value_counts_format, i);
+		data_file.nan_value_counts = GetCounts("nan_value_counts", nan_value_counts_format, i);
+
+		if (referenced_data_file) {
+			string_t val;
+			if (ReadOptionalField<string_t>(referenced_data_file_format, i, val)) {
+				data_file.referenced_data_file = val.GetString();
+			}
 		}
-		if (content_offset && FlatVector::Validity(*content_offset).RowIsValid(index)) {
-			data_file.content_offset = content_offset->GetValue(index);
+		if (content_offset) {
+			int64_t val;
+			if (ReadOptionalField<int64_t>(content_offset_format, i, val)) {
+				data_file.content_offset = Value::BIGINT(val);
+			} else {
+				data_file.content_offset = Value(LogicalType::BIGINT);
+			}
 		}
-		if (content_size_in_bytes && FlatVector::Validity(*content_size_in_bytes).RowIsValid(index)) {
-			data_file.content_size_in_bytes = content_size_in_bytes->GetValue(index);
+		if (content_size_in_bytes) {
+			int64_t val;
+			if (ReadOptionalField<int64_t>(content_size_in_bytes_format, i, val)) {
+				data_file.content_size_in_bytes = Value::BIGINT(val);
+			} else {
+				data_file.content_size_in_bytes = Value(LogicalType::BIGINT);
+			}
 		}
 
-		data_file.split_offsets = GetSplitOffsets(split_offsets, index);
-		data_file.has_sort_order_id = sort_order_id_validity.RowIsValid(index);
-		if (data_file.has_sort_order_id) {
-			data_file.sort_order_id = sort_order_id_data[index];
+		data_file.split_offsets = GetSplitOffsets(split_offsets_format, i);
+		int32_t sort_order_id;
+		if (ReadOptionalField<int32_t>(sort_order_id_format, i, sort_order_id)) {
+			data_file.sort_order_id = sort_order_id;
 		}
 		if (iceberg_version >= 2) {
-			data_file.content = (IcebergManifestEntryContentType)content_data[index];
-			data_file.equality_ids = GetEqualityIds(equality_ids, index);
+			data_file.content =
+			    (IcebergManifestEntryContentType)ReadRequiredField<int32_t>("content", content_format, i);
+			data_file.equality_ids = GetEqualityIds(equality_ids_format, i);
 
-			if (sequence_number_validity.RowIsValid(index)) {
-				entry.SetSequenceNumber(sequence_number_data[index]);
+			int64_t sequence_number;
+			if (ReadOptionalField<int64_t>(sequence_number_format, i, sequence_number)) {
+				entry.SetSequenceNumber(sequence_number);
 			}
-			if (file_sequence_number_validity.RowIsValid(index)) {
-				entry.SetFileSequenceNumber(file_sequence_number_data[index]);
+			int64_t file_sequence_number;
+			if (ReadOptionalField<int64_t>(file_sequence_number_format, i, file_sequence_number)) {
+				entry.SetFileSequenceNumber(file_sequence_number);
 			}
 		} else {
 			//! SPEC: Data file field content must default to 0 (data)
 			data_file.content = IcebergManifestEntryContentType::DATA;
 		}
 		if (iceberg_version >= 3) {
-			if (first_row_id_validity->RowIsValid(index)) {
-				data_file.SetFirstRowId(first_row_id_data[index]);
+			int64_t first_row_id;
+			if (ReadOptionalField<int64_t>(first_row_id_format, i, first_row_id)) {
+				data_file.SetFirstRowId(first_row_id);
 			}
 		}
 
-		if (snapshot_id_validity.RowIsValid(index)) {
-			entry.SetSnapshotId(snapshot_id_data[index]);
+		int64_t snapshot_id;
+		if (ReadOptionalField<int64_t>(snapshot_id_format, i, snapshot_id)) {
+			entry.SetSnapshotId(snapshot_id);
 		}
+
 		for (auto &it : partition_vectors) {
 			auto field_id = it.first;
+			auto &partition_vector = it.second.get();
 			for (auto &id_spec : partition_specs) {
 				auto &spec = id_spec.second;
 				for (auto &field : spec.fields) {
 					if (field_id == field.partition_field_id) {
-						auto &partition_vector = it.second.get();
-
 						IcebergPartitionInfo info;
-						// info.name = field.name;
 						info.field_id = static_cast<uint64_t>(field_id);
-						info.value = partition_vector.GetValue(index);
-						// info.source_id = field.source_id;
-						// info.transform = field.transform;
+						info.value = partition_vector.GetValue(i);
 						data_file.partition_info.push_back(std::move(info));
 					}
 				}
