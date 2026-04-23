fe9380d0f93f69e25cf5c4b2f7f474ea2f3767de
Yan <60644013+yan-alex@users.noreply.github.com>
2026-04-23 11:46:44 +0200

    do request in function not in bind

--- src/function/metadata/iceberg_load_table_response.cpp
+++ src/function/metadata/iceberg_load_table_response.cpp
@@ -18,17 +18,15 @@ using namespace duckdb_yyjson;
 
 namespace duckdb {
 
-struct StorageCredentialData {
-	string prefix;
-	case_insensitive_map_t<string> config;
-};
-
 struct IcebergLoadTableResponseBindData : public TableFunctionData {
-	string metadata_location;
-	string metadata_json;
-	case_insensitive_map_t<string> config;
-	vector<StorageCredentialData> storage_credentials;
-	string request_url;
+	IcebergTableEntry &table_entry;
+	IcebergCatalog &ic_catalog;
+	IcebergSchemaEntry &ic_schema;
+
+	IcebergLoadTableResponseBindData(IcebergTableEntry &table_entry, IcebergCatalog &ic_catalog,
+	                                 IcebergSchemaEntry &ic_schema)
+	    : table_entry(table_entry), ic_catalog(ic_catalog), ic_schema(ic_schema) {
+	}
 };
 
 struct IcebergLoadTableResponseGlobalState : public GlobalTableFunctionState {
@@ -39,7 +37,33 @@ struct IcebergLoadTableResponseGlobalState : public GlobalTableFunctionState {
 	}
 };
 
-static unique_ptr<HTTPResponse> MakeRequest(ClientContext &context, TableFunctionBindInput &input) {
+static unique_ptr<HTTPResponse> MakeRequest(ClientContext &context, const IcebergLoadTableResponseBindData &bind_data) {
+	auto &ic_catalog = bind_data.ic_catalog;
+	auto &ic_schema = bind_data.ic_schema;
+	auto &ic_table_entry = bind_data.table_entry;
+
+	auto url_builder = ic_catalog.GetBaseUrl();
+	url_builder.AddPrefixComponent(ic_catalog.prefix, ic_catalog.prefix_is_one_component);
+	url_builder.AddPathComponent(IRCPathComponent::RegularComponent("namespaces"));
+	url_builder.AddPathComponent(IRCPathComponent::NamespaceComponent(ic_schema.namespace_items));
+	url_builder.AddPathComponent(IRCPathComponent::RegularComponent("tables"));
+	url_builder.AddPathComponent(IRCPathComponent::RegularComponent(ic_table_entry.name));
+
+	HTTPHeaders headers(*context.db);
+	if (ic_catalog.attach_options.access_mode == IRCAccessDelegationMode::VENDED_CREDENTIALS) {
+		headers.Insert("X-Iceberg-Access-Delegation", "vended-credentials");
+	}
+	unique_ptr<HTTPResponse> response =
+	    ic_catalog.auth_handler->Request(RequestType::GET_REQUEST, context, url_builder, headers);
+	if (!response->Success()) {
+		throw IOException("GET request to '%s' failed with status %s: %s", url_builder.GetURLEncoded(),
+		                  EnumUtil::ToString(response->status), response->body);
+	}
+	return response;
+}
+
+static unique_ptr<FunctionData> IcebergLoadTableResponseBind(ClientContext &context, TableFunctionBindInput &input,
+                                                             vector<LogicalType> &return_types, vector<string> &names) {
 	auto input_string = input.inputs[0].ToString();
 	auto qualified_name = QualifiedName::ParseComponents(input_string);
 
@@ -64,68 +88,7 @@ static unique_ptr<HTTPResponse> MakeRequest(ClientContext &context, TableFunctio
 	auto &ic_catalog = table_entry.catalog.Cast<IcebergCatalog>();
 	auto &ic_schema = table_entry.schema.Cast<IcebergSchemaEntry>();
 
-	// Make the GET request
-	auto url_builder = ic_catalog.GetBaseUrl();
-	url_builder.AddPrefixComponent(ic_catalog.prefix, ic_catalog.prefix_is_one_component);
-	url_builder.AddPathComponent(IRCPathComponent::RegularComponent("namespaces"));
-	url_builder.AddPathComponent(IRCPathComponent::NamespaceComponent(ic_schema.namespace_items));
-	url_builder.AddPathComponent(IRCPathComponent::RegularComponent("tables"));
-	url_builder.AddPathComponent(IRCPathComponent::RegularComponent(table_entry.name));
-
-	HTTPHeaders headers(*context.db);
-	if (ic_catalog.attach_options.access_mode == IRCAccessDelegationMode::VENDED_CREDENTIALS) {
-		headers.Insert("X-Iceberg-Access-Delegation", "vended-credentials");
-	}
-	unique_ptr<HTTPResponse> response =
-	    ic_catalog.auth_handler->Request(RequestType::GET_REQUEST, context, url_builder, headers);
-	if (!response->Success()) {
-		throw IOException("GET request to '%s' failed with status %s: %s", url_builder.GetURLEncoded(),
-		                  EnumUtil::ToString(response->status), response->body);
-	}
-	return response;
-}
-
-static unique_ptr<FunctionData> IcebergLoadTableResponseBind(ClientContext &context, TableFunctionBindInput &input,
-                                                             vector<LogicalType> &return_types, vector<string> &names) {
-	auto ret = make_uniq<IcebergLoadTableResponseBindData>();
-
-	const unique_ptr<HTTPResponse> response = MakeRequest(context, input);
-
-	// Parse the response using yyjson
-	auto doc = ICUtils::APIResultToDoc(response->body);
-	auto *root = yyjson_doc_get_root(doc.get());
-
-	// Parse via LoadTableResult for the typed fields
-	auto load_result = rest_api_objects::LoadTableResult::FromJSON(root);
-
-	if (load_result.has_metadata_location) {
-		ret->metadata_location = load_result.metadata_location;
-	}
-
-	// Re-serialize the metadata sub-object as JSON
-	auto *metadata_val = yyjson_obj_get(root, "metadata");
-	if (metadata_val) {
-		auto *json_str = yyjson_val_write(metadata_val, 0, nullptr);
-		if (json_str) {
-			ret->metadata_json = json_str;
-			free(json_str);
-		}
-	}
-
-	if (load_result.has_config) {
-		ret->config = std::move(load_result.config);
-	}
-
-	if (load_result.has_storage_credentials) {
-		for (auto &cred : load_result.storage_credentials) {
-			StorageCredentialData data;
-			data.prefix = cred.prefix;
-			data.config = cred.config;
-			ret->storage_credentials.push_back(std::move(data));
-		}
-	}
-
-	ret->request_url = response->url;
+	auto ret = make_uniq<IcebergLoadTableResponseBindData>(table_entry, ic_catalog, ic_schema);
 
 	// metadata_location
 	names.push_back("metadata_location");
@@ -172,44 +135,59 @@ static void OutputMap(const case_insensitive_map_t<string> &config, Vector &conf
 }
 
 static void IcebergLoadTableResponseFunction(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
-	// contains the actual data
 	auto &bind_data = data.bind_data->Cast<IcebergLoadTableResponseBindData>();
 	auto &global_state = data.global_state->Cast<IcebergLoadTableResponseGlobalState>();
 
-	// We do this to signal let the engine know we have only this row to produce
 	if (global_state.done) {
 		return;
 	}
 	global_state.done = true;
+
+	auto response = MakeRequest(context, bind_data);
+
+	// Parse the response using yyjson
+	auto doc = ICUtils::APIResultToDoc(response->body);
+	auto *root = yyjson_doc_get_root(doc.get());
+
+	auto load_result = rest_api_objects::LoadTableResult::FromJSON(root);
+
 	output.SetCardinality(1);
 
 	// metadata_location
 	auto &metadata_location_vector = output.data[0];
 	FlatVector::GetData<string_t>(metadata_location_vector)[0] =
-	    StringVector::AddString(metadata_location_vector, bind_data.metadata_location);
+	    StringVector::AddString(metadata_location_vector, load_result.metadata_location);
 
 	// metadata (VARIANT)
 	auto &metadata_vector = output.data[1];
 	{
-		Vector json_vec(LogicalType::JSON(), 1);
-		FlatVector::GetData<string_t>(json_vec)[0] = StringVector::AddString(json_vec, bind_data.metadata_json);
-		VectorOperations::Cast(context, json_vec, metadata_vector, 1);
+		auto *metadata_val = yyjson_obj_get(root, "metadata");
+		if (metadata_val) {
+			auto *json_str = yyjson_val_write(metadata_val, 0, nullptr);
+			if (json_str) {
+				Vector json_vec(LogicalType::JSON(), 1);
+				FlatVector::GetData<string_t>(json_vec)[0] = StringVector::AddString(json_vec, string(json_str));
+				free(json_str);
+				VectorOperations::Cast(context, json_vec, metadata_vector, 1);
+			}
+		}
 	}
 
 	// config MAP(VARCHAR, VARCHAR)
 	auto &config_vector = output.data[2];
-	OutputMap(bind_data.config, config_vector);
+	OutputMap(load_result.config, config_vector);
 
 	// storage_credentials LIST(STRUCT(prefix, config))
 	auto &storage_credentials_vector = output.data[3];
-	auto cred_count = bind_data.storage_credentials.size();
+	auto &storage_credentials = load_result.storage_credentials;
+	auto cred_count = storage_credentials.size();
 	ListVector::Reserve(storage_credentials_vector, cred_count);
 	auto &cred_entry = ListVector::GetEntry(storage_credentials_vector);
 	auto &prefix_vec = StructVector::GetEntries(cred_entry)[0];
 	auto &cred_config_vec = StructVector::GetEntries(cred_entry)[1];
 
 	for (idx_t struct_idx = 0; struct_idx < cred_count; struct_idx++) {
-		auto &cred = bind_data.storage_credentials[struct_idx];
+		auto &cred = storage_credentials[struct_idx];
 
 		// prefix
 		FlatVector::GetData<string_t>(*prefix_vec)[struct_idx] = StringVector::AddString(*prefix_vec, cred.prefix);
@@ -242,7 +220,7 @@ static void IcebergLoadTableResponseFunction(ClientContext &context, TableFuncti
 	// request_url
 	auto &request_endpoint_vector = output.data[4];
 	FlatVector::GetData<string_t>(request_endpoint_vector)[0] =
-	    StringVector::AddString(metadata_location_vector, bind_data.request_url);
+	    StringVector::AddString(request_endpoint_vector, response->url);
 }
 
 TableFunctionSet IcebergFunctions::GetIcebergLoadTableResponseFunction() {
