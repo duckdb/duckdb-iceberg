a85963dce341b2b45ca1835b72e3928c3e330dc3
Yan <60644013+yan-alex@users.noreply.github.com>
2026-04-28 16:44:25 +0200

    remove unused property

--- src/include/catalog/rest/catalog_entry/schema/iceberg_schema_information.hpp
+++ src/include/catalog/rest/catalog_entry/schema/iceberg_schema_information.hpp
@@ -6,7 +6,6 @@ namespace duckdb {
 struct IcebergSchemaInformation {
 	case_insensitive_map_t<string> properties;
 	bool properties_loaded = false;
-	timestamp_t properties_expire_timestamp;
 };
 
 } // namespace duckdb
